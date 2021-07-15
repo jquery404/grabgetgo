2c56c6ce9eacf180b01342c90dafe8e4b8ae3373
Faisal Zaman <zamanfa@staff.vuw.ac.nz>
2021-07-15 16:12:15 +1200

    trying to make bidirectional audio work

--- client/assets/scripts/viewervr.js
+++ client/assets/scripts/viewervr.js
@@ -1,6 +1,7 @@
 let socket;
 let peer;
 let broadcasterStream;
+let audioStream;
 const video = document.querySelector("video");
 
 function init() {
@@ -41,6 +42,28 @@ function init() {
     })
 
 }
+
+
+async function getStream() {
+  const audioSource = audioSelect.value;
+  const constraints = {
+    audio: { deviceId: audioSource ? { exact: audioSource } : undefined, echoCancellation: true },
+    video: false
+  };
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    return gotStream(stream);
+  }
+  catch (error) {
+    return handleError(error);
+  }
+}
+
+function gotStream(stream) {
+  audioStream = stream;
+  socket.emit("broadcaster");
+} 
+
 
 function toggleMute() {
   
